92b3b4c01f67f4b85be44d5cd55146e0c49214fe
nathanbuyr <G00421082@atu.ie>
2026-04-19 02:55:37 +0100

    Changed vite config for render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig({
-base: "/GaitRehab_CSV_Viewer/",
-plugins: [react()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "")
+	const base = env.VITE_BASE_PATH || "/"
+
+	return {
+		base,
+		plugins: [react()],
+	}
 })
